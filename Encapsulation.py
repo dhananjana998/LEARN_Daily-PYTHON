12d7c0ac0d7597b7ea95ac8f0cc101e37da72a09
dhananjana998 <149720690+dhananjana998@users.noreply.github.com>
2025-07-15 23:46:14 +0530

    Encapsulation

---
+++ Encapsulation.py
@@ -0,0 +1,12 @@
+class my_one:
+    def firstName(self):
+        print("My First Name Sachini")
+        self.__lastName()  # access to the private variable
+
+    def __lastName(self):  #private variable
+        print("My Last Name Dhananjana")
+
+
+
+myObj = my_one()     #create object
+myObj.firstName()
