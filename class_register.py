cdb18667d9c8caa5d49f12134c28a65fb66784e0
D.G.S.S.Dhananjana <149720690+dhananjana998@users.noreply.github.com>
2025-07-22 22:59:00 +0530

    Update class_register.py

--- class_register.py
+++ class_register.py
@@ -1,33 +1,25 @@
 s_num = 1
-present = 0
+present = 0  
 absence = 0
 
-try:
-    Total_Student = int(input("Enter total number of students in class: "))
+Total_Student = int(input("Enter Total number student in class: ")) 
 
-    while s_num <= Total_Student:
-        In_class = input(f"Is student {s_num} in class (yes=1 / no=0): ")
+while s_num <= Total_Student:
+    In_class = input(f"Is student {s_num} in class (yes=1/no=0): ")
+    
+    
+    if In_class == "1":
+        print(f"{s_num}: 1 (Present)")
+        present += 1 
+    else:
+        print(f"{s_num}: 0 (Absent)")
+        absence += 1 
+    
+    s_num += 1
 
-        if In_class == "1":
-            print(f"{s_num}: 1")
-            present += 1
-        else:
-            print(f"{s_num}: 0")
-            absence += 1
+print(f"present : {present}, absence: {absence}") 
 
-        s_num += 1
-
-    print(f"present: {present}, absence: {absence}")
-
-except EOFError:
-    print("Error: No more input available. Please provide all required inputs.")
-    # You might want to exit here or handle it differently
-except ValueError:
-    print("Invalid input. Please enter a valid number for total students or 1/0 for presence.")
-
-
-
-    '''Enter total number of students in class: 5
+''' Enter total number of students in class: 5
 Is student 1 in class (yes=1 / no=0): 0
 1: 0
 Is student 2 in class (yes=1 / no=0): 1
@@ -38,4 +30,6 @@ Is student 4 in class (yes=1 / no=0): 1
 4: 1
 Is student 5 in class (yes=1 / no=0): 1
 5: 1
-present: 3, absence: 2'''
+present: 3, absence: 2
+
+Process finished with exit code 0'''
