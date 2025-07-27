d7f90d95e5d24125755ccc028650683634d72220
D.G.S.S.Dhananjana <149720690+dhananjana998@users.noreply.github.com>
2025-07-27 23:05:15 +0530

    File_Handling_PDF_Viewer.py

--- File_Handling_Note_saver.py
+++ File_Handling_Note_saver.py
@@ -1,7 +1,7 @@
 import os         # module of the make connection with the device os
 import webbrowser
 #  Use the Documents folder
-folder_path = os.path.expanduser("~/Documents")
+folder_path = os.path.expanduser("~/Documents/Sample")
 
 # List to store (file name, full path)
 pdf_files = []
@@ -18,9 +18,9 @@ for root, dirs, files in os.walk(folder_path):# open   main path Documents
 if not pdf_files:
     print("No PDF files found .")
 else:
-    print(" PDF Files  in Downloads:")
+    print(" -----------------------------------PDF Files  in Sample Folder-----------------------------------\n")
     for i, (name,_) in enumerate(pdf_files, start=1):
-        print(f"{i}. {name}")
+        print(f"\t\t\t {i}. {name}")
 
     # User selects one to open
     while True:
@@ -30,44 +30,27 @@ else:
         if choice.isdigit():
             index = int(choice) - 1
             if 0 <= index < len(pdf_files):
-                print(f"Open file: {pdf_files[index][0]}")
+                print(f"\n Open file: {pdf_files[index][0]}")
                 webbrowser.open(pdf_files[index][1])
             else:
                 print(" Invalid number.")
         else:
-            print(" Please enter a number or 'exit'.")
+            print(" Please enter a number or 'exit'.\n")
 
-'''     out put
- PDF Files  in Downloads:
-1. 10.2478_bjreecm-2023-0015
-2. 2020-2021 EEI4366-EEX4366
-3. 222513158_EEI4366_TMA2
-4. 222513158_MHZ3459
-5. 46-T15091
-6. 47-S15110
-7. BookDao
-8. Current status of construction contractors in time of crisis and implications on economy _ Daily FT
-9. Grade 9 evaluation[1]anex
-10. L5 WD_protected
-11. LargeScaleContractors_KandyConference_14th
-12. MHZ4256
-13. MSME-Presentation-English
-14. MSMEs_Report (1).pdf (1).crdownload
-15. oop QA Note
-16. S22009883
-17. S22009883_4369Lab1
-18. S22009883_4369Lab4
-19. S22009883_EEI4369_First_Progress_Review
-20. s22009883_Miniproject_4465
-21. SRI LANKA One million Sri Lankan construction workers have lost their jobs
-22. todaypri2
-23. volume-2-online-333-343 (4)
-24. x
-25. 222513158_MP
-26. GUIslice_ref
-27. 222513158_EEI4366_TMA2
+''' 
 
-Enter PDF number to open (or exit to enter 'exit' ): 3
-Opening: 222513158_EEI4366_TMA2
+ -----------------------------------PDF Files  in Sample Folder-----------------------------------
 
-Enter PDF number to open (or exit to enter 'exit' ): '''
+			 1. Sample File 01
+			 2. Sample File 02
+			 3. Sample File 03
+
+Enter PDF number to open (or exit to enter 'exit' ): 1
+
+ Open file: Sample File 01
+
+Enter PDF number to open (or exit to enter 'exit' ): exit
+
+Process finished with exit code 0
+
+'''
