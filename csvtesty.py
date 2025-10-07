8e02583182d3dd063faeded69b22ed48a8a6c378
D.G.S.S.Dhananjana <149720690+dhananjana998@users.noreply.github.com>
2025-10-08 00:13:11 +0530

    Update csvtesty.py

--- csvtesty.py
+++ csvtesty.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-# Use raw string (r) or double backslashes for Windows file path
+
 dataFrame = pd.read_csv(r"C:\Users\User\Desktop\python_practice\sample\Book1.csv")
 
-# Display the DataFrame
+
+
 print(dataFrame)
